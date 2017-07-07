cced6240207e36a75c8e275f2026dbf23d05e35c
root <root@spark_mandrill.lan>
2017-07-07 16:34:15 +0000

    removed bash scripts that introduced redundancy

--- sendNodeMetrics.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#secondary start script that runs the python code to connect to RabbitMQ and send the data
-
-sudo python /root/waggleWebAPI/sendData.py
--- startWebApp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#starts the python script to run the local web app
-
-sudo python /root/waggleWebAPI/waggleApp.py
-
