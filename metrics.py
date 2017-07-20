4ccdadc2fa8f29d851980e7c0de60d189f0b93bc
Adam Morrison <adammorr@adammorr-desktop.lan>
2017-07-20 15:17:38 -0500

    last few updates to metrics.py and removes test files from repo

--- metrics.py
+++ metrics.py
@@ -10,9 +10,8 @@ import re
 
 def getGeneralInfo():
     #get general info about node using 'hostnamectl'
-    getHostName = str(subprocess.check_output('hostnamectl', shell=True))
+    getHostName = str(subprocess.check_output('hostnamectl', shell=True).decode('ascii'))
     pattern = r"\s*[a-zA-Z]*:\s*.\n|\s*[a-zA-Z]*\s*[a-zA-Z]*:\s*.*\n"
-
     info = re.findall(pattern,getHostName)
 
     genInfoList = {}
@@ -42,9 +41,8 @@ def getNodeUptime():
 
 def getUSBDevs():
     #get USB devices that are connected to node
-    getDevs = str(subprocess.check_output('lsusb',shell=True))
+    getDevs = str(subprocess.check_output('lsusb',shell=True).decode('ascii'))
     pattern = r"Bus\s*\d+\s*Device\s*\d+:\s*ID.+"
-
     info = re.findall(pattern,getDevs)
     devs = {}
 
@@ -62,45 +60,52 @@ def getMemInfo():
     memInfoList = {}
         
     #get info from /proc/meminfo file
-    memFile = open('/proc/meminfo')
-    fileData = memFile.readlines()
+    getInfo = str(subprocess.check_output('cat /proc/meminfo', shell=True).decode('ascii'))
 
+    memFreeRE = r"MemFree:\s*.*\n"
+    #memAvailableRE = r"MemAvailable:\s*.*\n"
+    memTotalRE = r"MemTotal:\s*.*\n"
+   
+    memFreeInfo = re.findall(memFreeRE,getInfo)
+    #memAvailableInfo = re.findall(memAvailableRE,getInfo)
+    memTotalInfo = re.findall(memTotalRE,getInfo)
+    memFree = re.sub(r"MemFree:\s*","",memFreeInfo[0])
+    #memAvailable = re.sub(r"MemAvailable:\s*","",memAvailableInfo[0])
+    memTotal = re.sub(r"MemTotal:\s*","",memTotalInfo[0])
 
-    #for loop limits memInfoList to first 3 metrics but this can be changed
-    for i in range(0,3):
-        metricRE = r".*:"
-        valueRE = r":.*\n"
-        metric = str(re.findall(metricRE, fileData[i]))
-        #print metric
-        value = str(re.findall(valueRE, fileData[i]))
-        #print value
-        memInfoList.update({metric[0].replace(":","").replace(" ",""):value[0].replace(":","").replace(" ","").replace("\n","")})
-
+    memInfoList = {"MemFree":memFree,"MemTotal":memTotal}
     return memInfoList
 
 def getCPUInfo():
     #get CPU info from node
-    getInfo = str(subprocess.check_output('cat /proc/cpuinfo', shell=True))
-
+    getInfo = str(subprocess.check_output('cat /proc/cpuinfo', shell=True).decode('ascii'))
     coreRE = r"cpu cores\s*:.*"
     vendorRE = r"vendor_id\s*:.*"
     modelRE = r"model name\s*:.*"
+    
+    if re.search(vendorRE,getInfo)() #if statements to be removed (for testing purposes)
+        vendorInfo = str(re.search(vendorRE,getInfo).group(0))
+        cpuInfo = str(re.search(coreRE,getInfo).group(0))
+        modelInfo = str(re.search(modelRE,getInfo).group(0))
 
-    vendorInfo = str(re.search(vendorRE,getInfo).group(0))
-    cpuInfo = str(re.search(coreRE,getInfo).group(0))
-    modelInfo = str(re.search(modelRE,getInfo).group(0))
+        cpuCores = re.sub(r"cpu cores\s*:\s*", "", cpuInfo)
+        vendorId = re.sub(r"vendor_id\s*:\s*", "", vendorInfo)
+        modelName = re.sub(r"model name\s*:\s*", "", modelInfo)
 
-    cpuCores = re.sub(r"cpu cores\s*:\s*", "", cpuInfo)
-    vendorId = re.sub(r"vendor_id\s*:\s*", "", vendorInfo)
-    modelName = re.sub(r"model name\s*:\s*", "", modelInfo)
+        cpuInfo = {"CPUCores":cpuCores,"VendorID":vendorId,"ModelName":modelName}
+    else if re.findall(r"Processor\s*:\s*.*",getInfo)
+        procInfo = re.findall(r"Processor\s*:\s*.*",getInfo)
 
-    cpuInfo = {"CPUCores":cpuCores,"VendorID":vendorId,"ModelName":modelName}
+        procOne = procInfo[0]
+        proc = re.sub(r".*:","",procOne)
+
+        cpuInfo = {"Processor":str(proc)}
 
     return cpuInfo
 
 def getDiskInfo():
     #get information about disks on node
-    getInfo = str(subprocess.check_output('~/waggleWebAPI/detectDiskDevices.sh', shell=True))
+    getInfo = str(subprocess.check_output('~/waggleWebAPI/detectDiskDevices.sh', shell=True).decode('ascii'))
 
     pattern = r".*memory card not recognized.*"
     if (re.search(pattern, getInfo)):
@@ -112,10 +117,10 @@ def getDiskInfo():
         currentDiskFree = "test"
         currentDiskUsage = "test"
     else:
-        currDev = str(re.findall(r"CURRENT_DISK_DEVICE_NAME=.*",getInfo))
-        otherDev = str(re.findall(r"OTHER_DISK_DEVICE_NAME=.*",getInfo))
-        currType = str(re.findall(r"CURRENT_DISK_DEVICE_TYPE=.*",getInfo))
-        otherType = str(re.findall(r"OTHER_DISK_DEVICE_TYPE=.*",getInfo))
+        currDev = re.findall(r"CURRENT_DISK_DEVICE_NAME=.*",getInfo)
+        otherDev = re.findall(r"OTHER_DISK_DEVICE_NAME=.*",getInfo)
+        currType = re.findall(r"CURRENT_DISK_DEVICE_TYPE=.*",getInfo)
+        otherType = re.findall(r"OTHER_DISK_DEVICE_TYPE=.*",getInfo)
 
         currDiskName = currDev[0].replace("CURRENT_DISK_DEVICE_NAME=","")
         otherDiskName = otherDev[0].replace("OTHER_DISK_DEVICE_NAME=","")
@@ -126,7 +131,6 @@ def getDiskInfo():
         currentDiskFree = "to be implemented"
         currentDiskUsage = "to be implemented"
 
-    #diskInfo = {"CurrDiskName":currDiskName,"OtherDiskName":otherDiskName,"CurrDiskType":currDiskType,"OtherDiskType":otherDiskType}
     diskInfo = {"CurrentDiskName":currDiskName,"OtherDiskName":otherDiskName,"CurrentDiskType":currDiskType,"OtherDiskType":otherDiskType,"CurrentDiskUsed":currentDiskUsed,"CurrentDiskFree":currentDiskFree,"CurrentDiskUsage":currentDiskUsage}
     return diskInfo
 
@@ -135,41 +139,36 @@ def getRunningServices():
     statusList = []
     services = {}
 
-    getInfo = str(subprocess.check_output('systemctl|grep running',shell=True))
-    
-    getServices = r".*\.service"
+    getInfo = str(subprocess.check_output("systemctl|grep '.service.*running'",shell=True).decode('ascii'))
+    getServices = r".*[.]service"
 
     #All running services
-    runningServices = str(re.findall(getServices,getInfo))
+    runningServices = re.findall(getServices,getInfo)
 
     #Get current time in UTC
     currTime = pytz.utc.localize(datetime.datetime.utcnow().replace(microsecond=0))
 
     #find uptime for each running service
     for i in runningServices:
-        getStatus = "systemctl status " + i
+        getStatus = "systemctl status " + str(i)
         active = r"\s*Active:\s*.*"
         time = r"since\s.*;"
         
         #retreive the status of each service
         status = str(subprocess.check_output(getStatus,shell=True))
-        statusList.append(status)
-        
+                
         #parse out the line containing "Active: " and parse out the start time of the service
-        findActive = str(re.findall(active,status))
-        findTime = str(re.findall(time,findActive[0]))
+        findActive = re.findall(active,status)
+        findTime = re.findall(time,findActive[0])
         
-        if findTime:
-            #Format the service start time and put it in UTC
-            ufStartTime = parse(findTime[0].replace("since ","").replace(";",""))
-            startTime = ufStartTime.astimezone(pytz.timezone("UTC"))
-            
-            #Subtract the two values to get the run time of the process
-            uptime = str(currTime - startTime)
-            services.update({i:uptime})
-        else:
-            services.update({"Error":"error in getRunningServices()"})
-
+        #Format the service start time and put it in UTC
+        ufStartTime = parse(findTime[0].replace("since ","").replace(";",""))
+        startTime = ufStartTime.astimezone(pytz.timezone("UTC"))
+           
+        #Subtract the two values to get the run time of the process
+        uptime = str(currTime - startTime)
+        services.update({i:uptime})
+  
     return services
 
 def getSystemdServices(): #to be deprecated
@@ -186,7 +185,7 @@ def getNodeID():
     #get the node ID (name)
     hostInfo = str(subprocess.check_output('hostnamectl', shell=True))
     pattern = r"Static\s+hostname:.+\n"
-    hostName = str(re.findall(pattern,hostInfo))
+    hostName = re.findall(pattern,hostInfo)
 
     ID = hostName[0].replace("Static hostname: ","").replace("\n","")
 
@@ -206,13 +205,13 @@ def sendMetrics():
     return json.dumps(jsonData)
 
 if __name__ == "__main__":
-    print "getGeneralInfo():"+"\n"+str(getGeneralInfo())+"\n"
-    print "getNodeUptime():"+"\n"+str(getNodeUptime())+"\n"
-    print "getUSBDevs():"+"\n"+str(getUSBDevs())+"\n"
-    print "getMemInfo():"+"\n"+str(getMemInfo())+"\n"
-    print "getCPUInfo():"+"\n"+str(getCPUInfo())+"\n"
-    print "getDiskInfo():"+"\n"+str(getDiskInfo())+"\n"
-    print "getRunningServices():"+"\n"+str(getRunningServices())+"\n"
-    #print "getSystemdServices():","\n",getSystemdServices(),"\n"
-    print "getNodeID():"+"\n"+str(getNodeID())+"\n"
-    #print "sendMetrics():","\n",sendMetrics(),"\n"
+    print ("getGeneralInfo():"+"\n"+str(getGeneralInfo())+"\n")
+    print ("getNodeUptime():"+"\n"+str(getNodeUptime())+"\n")
+    print ("getUSBDevs():"+"\n"+str(getUSBDevs())+"\n")
+    print ("getMemInfo():"+"\n"+str(getMemInfo())+"\n")
+    print ("getCPUInfo():"+"\n"+str(getCPUInfo())+"\n")
+    print ("getDiskInfo():"+"\n"+str(getDiskInfo())+"\n")
+    print ("getRunningServices():"+"\n"+str(getRunningServices())+"\n")
+    #print ("getSystemdServices():","\n",getSystemdServices(),"\n")
+    print ("getNodeID():"+"\n"+str(getNodeID())+"\n")
+    #print ("sendMetrics():","\n",sendMetrics(),"\n")
