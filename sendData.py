618151a6bd9e1831c4e75c23f5773dd13e091dd1
Adam Morrison <adammorr@adammorr-desktop.lan>
2017-08-01 10:27:24 -0500

    fixes timestamp error in sendData.py

--- sendData.py
+++ sendData.py
@@ -4,9 +4,10 @@ import time
 import requests
 import datetime
 
+
 from metrics import sendMetrics, getGeneralInfo
 
-sampleRate = 3 #how often the node should be pinged for metrics (in seconds)
+sampleRate = 1 #how often the node should be pinged for metrics (in seconds)
 genInfo = getGeneralInfo()
 
 if genInfo['Architecture'] == 'arm':
@@ -38,16 +39,17 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 #print ('channel = {}'.format(channel))
 
-properties = pika.BasicProperties(
+while 1:
+    time.sleep(sampleRate)
+
+    properties = pika.BasicProperties(
             headers = {
                 'value' : 80, 
                 'reply_to' : "0000020000000000"},
             timestamp=int(datetime.datetime.utcnow().timestamp() * 1000),
             reply_to="0000020000000000")
-#print ('properties = {}'.format(properties))
-
-while 1:
-    time.sleep(sampleRate)
+    #print ('properties = {}'.format(properties))
+    
     jsonData = sendMetrics()
     print (str(jsonData))
     channel.basic_publish(exchange='node-metrics', 
